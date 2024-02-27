753d472d238fdd16d5d97b19bf45a547641ec689
Krystian Stasiowski <sdkrystian@gmail.com>
2024-02-27 10:24:50 -0500

    [FOLD] expand tests

--- clang/test/CXX/temp/temp.res/temp.dep/temp.dep.type/p6.cpp
+++ clang/test/CXX/temp/temp.res/temp.dep/temp.dep.type/p6.cpp
@@ -7,10 +7,16 @@ namespace N0 {
 
   struct A {
     int x; // expected-note 3{{candidate found by name lookup is 'N0::A::x'}}
+
+    void f(int); // expected-note 3{{candidate found by name lookup is 'N0::A::f'}}
+    void f(char); // expected-note 3{{candidate found by name lookup is 'N0::A::f'}}
   };
 
   struct B {
     char x; // expected-note 3{{candidate found by name lookup is 'N0::B::x'}}
+
+    void f(int); // expected-note 3{{candidate found by name lookup is 'N0::B::f'}}
+    void f(char); // expected-note 3{{candidate found by name lookup is 'N0::B::f'}}
   };
 
   template<typename T>
@@ -27,6 +33,18 @@ namespace N0 {
       this->B::x;
       this->C::x;
       this->T::x;
+
+      f(0);
+      A::f(0);
+      B::f(0);
+      C::f(0);
+      T::f(0);
+
+      this->f(0);
+      this->A::f(0);
+      this->B::f(0);
+      this->C::f(0);
+      this->T::f(0);
     }
 
     void instantiated() {
@@ -41,6 +59,18 @@ namespace N0 {
       this->B::x;
       this->C::x; // expected-error{{the result of lookup for 'x' in the template definition context differs from the result during instantiation}}
       this->T::x;
+
+      f(0);
+      A::f(0);
+      B::f(0);
+      C::f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      T::f(0);
+
+      this->f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      this->A::f(0);
+      this->B::f(0);
+      this->C::f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      this->T::f(0);
     }
   };
 
@@ -52,11 +82,20 @@ namespace N1 {
 
   struct A {
     int x;
+
+    void f(int);
+    void f(char);
   };
 
   struct B : virtual A {
     char x; // expected-note 3{{candidate found by name lookup is 'N1::B::x'}}
     bool y; // expected-note 2{{'B::y' declared here}}
+
+    void f(int); // expected-note 3{{candidate found by name lookup is 'N1::B::f'}}
+    void f(char); // expected-note 3{{candidate found by name lookup is 'N1::B::f'}}
+
+    void g(int); // expected-note 2{{'B::g' declared here}}
+    void g(char);
   };
 
   template<typename T>
@@ -85,6 +124,30 @@ namespace N1 {
       this->B::y;
       this->C::y;
       this->T::y;
+
+      f(0);
+      A::f(0);
+      B::f(0);
+      C::f(0);
+      T::f(0);
+
+      this->f(0);
+      this->A::f(0);
+      this->B::f(0);
+      this->C::f(0);
+      this->T::f(0);
+
+      g(0); // expected-error{{use of undeclared identifier 'g'}}
+      A::g(0); // expected-error{{no member named 'g' in 'N1::A'}}
+      B::g(0);
+      C::g(0);
+      T::g(0);
+
+      this->g(0);
+      this->A::g(0); // expected-error{{no member named 'g' in 'N1::A'}}
+      this->B::g(0);
+      this->C::g(0);
+      this->T::g(0);
     }
 
     void instantiated() {
@@ -92,6 +155,7 @@ namespace N1 {
       A::x;
       B::x;
       C::x; // expected-error{{the result of lookup for 'x' in the template definition context differs from the result during instantiation}}
+      T::x;
 
       this->x; // expected-error{{the result of lookup for 'x' in the template definition context differs from the result during instantiation}}
       this->A::x;
@@ -110,6 +174,30 @@ namespace N1 {
       this->B::y;
       this->C::y;
       this->T::y;
+
+      f(0);
+      A::f(0);
+      B::f(0);
+      C::f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      T::f(0);
+
+      this->f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      this->A::f(0);
+      this->B::f(0);
+      this->C::f(0); // expected-error{{the result of lookup for 'f' in the template definition context differs from the result during instantiation}}
+      this->T::f(0);
+
+      g(0); // expected-error{{use of undeclared identifier 'g'}}
+      A::g(0); // expected-error{{no member named 'g' in 'N1::A'}}
+      B::g(0);
+      C::g(0);
+      T::g(0);
+
+      this->g(0);
+      this->A::g(0); // expected-error{{no member named 'g' in 'N1::A'}}
+      this->B::g(0);
+      this->C::g(0);
+      this->T::g(0);
     }
   };
 
