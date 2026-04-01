19b6f2ccc8683dc343dfdf8fe1a648e16f0f575e
Krystian Stasiowski <sdkrystian@gmail.com>
2026-03-31 20:16:25 -0400

    Add missing arguments check

--- clang/lib/Sema/SemaModule.cpp
+++ clang/lib/Sema/SemaModule.cpp
@@ -1654,6 +1654,9 @@ void Sema::ActOnModuleImportAttrs(Decl *D,
 
   for (const auto &AL : Attrs) {
     if (AL.getKind() == ParsedAttr::AT_ProfilesRequire) {
+      if (!AL.checkAtLeastNumArgs(*this, 1))
+        continue;
+
       if (!ImportedMod) {
         Diag(AL.getLoc(), diag::err_profiles_require_not_on_import);
         continue;
