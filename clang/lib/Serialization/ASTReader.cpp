1d0b5a803cb21b96ba22cc2174856e5ed99ec50c
Krystian Stasiowski <sdkrystian@gmail.com>
2026-04-06 16:47:53 -0400

    Don't reparse profile names

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -6656,11 +6656,16 @@ Module *ASTReader::getSubmodule(uint32_t GlobalID) {
     case SUBMODULE_ENFORCED_PROFILES: {
       unsigned Idx = 0;
       while (Idx < Record.size()) {
-        unsigned Len = Record[Idx++];
+        unsigned NameLen = Record[Idx++];
+        std::string Name(Record.begin() + Idx,
+                         Record.begin() + Idx + NameLen);
+        Idx += NameLen;
+        unsigned DesigLen = Record[Idx++];
         std::string Desig(Record.begin() + Idx,
-                          Record.begin() + Idx + Len);
-        Idx += Len;
-        CurrentModule->EnforcedProfileDesignators.push_back(std::move(Desig));
+                          Record.begin() + Idx + DesigLen);
+        Idx += DesigLen;
+        CurrentModule->EnforcedProfileDesignators.push_back(
+            {std::move(Name), std::move(Desig)});
       }
       break;
     }
