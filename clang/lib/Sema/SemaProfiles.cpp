02f47fa8f916273bb738176002ad1178ddb7ef63
Krystian Stasiowski <sdkrystian@gmail.com>
2026-09-09 15:46:04 -0400

    Delete the std::init includes that no longer have a use

--- clang/lib/Sema/SemaProfiles.cpp
+++ clang/lib/Sema/SemaProfiles.cpp
@@ -27,8 +27,6 @@
 #include "clang/Basic/SourceManager.h"
 #include "clang/Sema/Initialization.h"
 #include "clang/Sema/ParsedAttr.h"
-#include "clang/Sema/Scope.h"
-#include "clang/Sema/ScopeInfo.h"
 #include "clang/Sema/Sema.h"
 #include "llvm/ADT/STLExtras.h"
 
